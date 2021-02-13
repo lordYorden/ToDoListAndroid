543624376a172ed01537fd61a0a4404c5518ebf0
yarden perets <yordenperets@gmail.com>
2021-02-13 16:12:28 +0200

    added new landscape layout and changed purple color and added storage perms

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -6,10 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.ImageDecoder;
-import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -77,13 +77,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public Drawable imageFromFile(String filePath) throws FileNotFoundException
+    public Bitmap imageFromFile(String filePath) throws FileNotFoundException
     {
         File imgFile = new File(filePath);
         if(imgFile.exists())
         {
-            ImageDecoder.Source source = ImageDecoder.createSource(imgFile);
-            Drawable drawable = ImageDecoder.decodeBitmap(source);
+            Bitmap myBitmap = BitmapFactory.decodeFile(filePath);
+            if(myBitmap != null)
+            {
+                return myBitmap;
+            }
+            else
+            {
+                throw new FileNotFoundException("Could not find " + filePath);
+            }
         }
         else
         {
